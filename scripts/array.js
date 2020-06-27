6ca35a21255bb70e8d1c05ccf4c19bc95a47b910
Pitsbeary <duomisiewitz@gmail.com>
2020-06-27 15:46:47 +0200

    alpha

--- scripts/array.js
+++ scripts/array.js
@@ -5,13 +5,11 @@ const SortingAlgorithms = {
 	
     BUBBLE: {	name: "Bubble Sort", 
 					link: "https://en.wikipedia.org/wiki/Bubble_sort",
-					desc: "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted"
-	},
+					desc: "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted" },
 				
     SELECT: {	name: "Select Sort", 
 					link: "https://en.wikipedia.org/wiki/Selection_sort",
-					desc: "The algorithm divides the input list into two parts: a sorted sublist of items which is built up from left to right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right."
-	},
+					desc: "The algorithm divides the input list into two parts: a sorted sublist of items which is built up from left to right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right." },
 	
     INSERT: {	name: "Insert Sort", 
 					link: "https://en.wikipedia.org/wiki/Insertion_sort", 
@@ -111,7 +109,7 @@ function createLink( linkText )
 
 function updateAlgorithmName()
 {
-	document.getElementById("algorithm-type").innerHTML = algorithm.name;
+	document.getElementById( "algorithm-type" ).innerHTML = algorithm.name;
 	document.getElementById( "algorithm-pick" ).innerHTML = algorithm.name;
 }
 
